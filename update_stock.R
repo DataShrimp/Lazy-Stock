bbc2fbd5ce5386d37ccb713b0293e4a8bf61e2af
Xu <xu@datashrimp.com>
2017-09-03 23:44:01 +0000

    google api update

--- update_stock.R
+++ update_stock.R
@@ -10,7 +10,7 @@ getStrFromUrl <- function(url) {
   ret <- gsub("\n|//", "", ret)
 }
 
-url1 <- "http://finance.google.com/finance/info?client=ig&q=NASDAQ%3AAAPL,GOOG,AMZN,FB,BIDU,BABA"
+url1 <- "http://finance.google.com/finance/info?client=ig&q=AAPL,GOOG,AMZN,FB,BIDU,BABA"
 url2 <- "http://finance.google.com/finance/info?client=ig&q=HKG%3A0700"
 
 df1 <- fromJSON(getStrFromUrl(url1))
@@ -26,7 +26,7 @@ df3$Close <- as.numeric(gsub(",","",df3$l))
 # save or not
 df1 <- read.csv('stock.csv')
 df1$Date <- as.Date(df1$Date)
-if (max(df$Date) == max(df1$Date)) {
+if (max(df3$Date) == max(df1$Date)) {
   print("no need to download")
 } else {
   df1 <- rbind(df1, df3[,c('Company','Date','Close')])
